21665a8d390d906128c616722753bf9041e88c13
lanyangli <lanyangli@DESKTOP-80HMKA8>
2018-09-04 02:31:24 +0800

    test

--- src/main/java/com/liulang/eureka/EurekaserverApplication.java
+++ src/main/java/com/liulang/eureka/EurekaserverApplication.java
@@ -3,7 +3,11 @@ package com.liulang.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+/**
+ * 服务注册中心
+ * @author lanyangli
+ *
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaserverApplication {
