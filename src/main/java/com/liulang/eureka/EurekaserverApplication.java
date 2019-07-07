fc41dd7d50bca1054a96fad437a86ddf07f14382
lanyangli <lanyangli@DESKTOP-80HMKA8>
2019-07-07 23:18:04 +0800

    ddd

--- src/main/java/com/liulang/eureka/EurekaserverApplication.java
+++ src/main/java/com/liulang/eureka/EurekaserverApplication.java
@@ -1,5 +1,8 @@
 package com.liulang.eureka;
 
+import java.lang.management.ManagementFactory;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -13,6 +16,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaserverApplication {
 
 	public static void main(String[] args) {
+		List<String> inputArguments = ManagementFactory.getRuntimeMXBean().getInputArguments();   
+	    System.out.println("===================java options=============== ");  
+	    System.out.println(inputArguments); 
 		SpringApplication.run(EurekaserverApplication.class, args);
 	}
 }
